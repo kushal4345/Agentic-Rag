9514c4b0f46ff5d36fc822b70ddd3fd90630c92b
kushal4345 <jeeultimate4345@gmail.com>
2025-07-22 18:58:49 +0530

    structured to the output

--- ChatModels/chains.py
+++ ChatModels/chains.py
@@ -2,8 +2,31 @@ from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 from langchain_openai import AzureChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
+from pydantic import BaseModel, Field
+from typing import Optional
+load_dotenv()
+
+class Roadmap(BaseModel):
+    domain: str
+    topic: str
+    roadmap: str
+    Time:Optional[int] = Field(None, description="Time required to complete the roadmap in months")
+
 
 prompt = PromptTemplate(
     template= "you are a helpfull {domain} assistant that will tell the roadmap of {topic} in a good format and with a good structure",
     input_variables = ["domain","topic"]
 )
+
+model = AzureChatOpenAI(
+    deployment_name="gpt-4o",  # Your Azure deployment name
+    openai_api_version="2025-01-01-preview",
+    temperature=0.7,
+    max_tokens= 500
+)
+structured_output = model.with_structured_output(Roadmap)
+parser = StrOutputParser()
+chain = prompt | model | parser | structured_output
+
+result = chain.invoke({"domain": "teacher", "topic": "generative AI"})
+print(result)
