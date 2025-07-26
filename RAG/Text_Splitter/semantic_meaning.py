5fbbab7cf7872010d602e38cc75b79dab423e137
kushal4345 <jeeultimate4345@gmail.com>
2025-07-26 22:06:31 +0530

    semantic meaning based splitting

---
+++ RAG/Text_Splitter/semantic_meaning.py
@@ -0,0 +1,14 @@
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+load_dotenv
+
+text_splitter = SemanticChunker(
+OpenAIEmbeddings() , breakpoint_threshold_type="standard deviation" , #deviation means comaparing each chunk and check similarity
+breakpoint_threshold_amount=1
+)
+
+sample = """
+write your text here
+"""
+
