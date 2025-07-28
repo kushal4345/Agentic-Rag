a4d3b2941a77ceda09848c62f493d0e2390f2150
kushal4345 <jeeultimate4345@gmail.com>
2025-07-29 00:16:27 +0530

    mini project youtube chatbot using rag

--- RAG/Projects/tempCodeRunnerFile.py
+++ RAG/Projects/tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-youtube_transcript_api
+# embedding_model = AzureOpenAIEmbeddings(
+# #     deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+# #     openai_api_key=os.getenv("EMBEDDING_MODEL_API_KEY"),
+# #     openai_api_version=os.getenv("EMBEDDING_MODEL_VERSION"),
+# #     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+# # )
--- RAG/Projects/youtubeChatbot.py
+++ RAG/Projects/youtubeChatbot.py
@@ -2,21 +2,18 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import AzureChatOpenAI 
 from langchain_openai import AzureOpenAIEmbeddings
-
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
 load_dotenv()  
-video_id = "640KMYtxCeI"
+video_id = "GDrBIKOR01c"
 ytt_api = YouTubeTranscriptApi()
 transcript = ytt_api.fetch(video_id)
-
+print(transcript)
 full_text = " ".join(snippet.text for snippet in transcript)
-
 # print(full_text)
-
-
+# print(full_text)
 
 
 # split the text into chunks 
@@ -24,31 +21,57 @@ full_text = " ".join(snippet.text for snippet in transcript)
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200 )
 chunks = splitter.create_documents([full_text])
 print(len(chunks))
-#initialize the embedding model
 
-api_key = os.getenv("embedding-model-Api_Key")
-endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-deployment = os.getenv("AZURE_OPENAI_DEPLOYMENT")
-api_version = os.getenv("EMBEDDING_MODEL_VERSION")
+print("--- Verifying Azure OpenAI Settings ---")
+print(f"Deployment Name (Embeddings): {os.getenv('AZURE_OPENAI_DEPLOYMENT')}")
+print(f"Azure Endpoint: {os.getenv('AZURE_OPENAI_ENDPOINT')}")
+print(f"API Version: {os.getenv('EMBEDDING_MODEL_VERSION')}")
+print(f"API Key Loaded: {os.getenv('EMBEDDING_MODEL_API_KEY') is not None}")
+
 
 embedding_model = AzureOpenAIEmbeddings(
-    deployment=deployment,
-    model="text-embedding-ada-002",  # Usually this
-    openai_api_base=endpoint,
-    openai_api_type="azure",
-    openai_api_key=api_key,
-    openai_api_version=api_version,
-    chunk_size=1000
-)
+        deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+        openai_api_key=os.getenv("EMBEDDING_MODEL_API_KEY"),
+        openai_api_version=os.getenv("EMBEDDING_MODEL_VERSION"),
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    )
+ 
 # generate the emebedding
 
-#store the embedding into vector db
+# # store the embedding into vector db
 vector_store= FAISS.from_documents(chunks,embedding_model,)
 vector_store.index_to_docstore_id
-#initialize the gpt 4 llm
+# #initialize the gpt 4 llm
 
-# retrieve the top 5 vector 
+# # retrieve the top 5 vector 
+retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 4})
+results = retriever.invoke("What are AI agents?")
 
-# pass it into model 
 
-# genearte the response
+model = AzureChatOpenAI(
+    deployment_name="gpt-4o",  # Your Azure deployment name
+    openai_api_version="2025-01-01-preview",
+    temperature=0.7,
+    max_tokens=500
+)
+
+prompt = PromptTemplate(
+    template="""
+      You are a helpful assistant.
+      Answer ONLY from the provided transcript context.
+      If the context is insufficient, just say you don't know.
+
+      {context}
+      Question: {question}
+    """,
+    input_variables = ['context', 'question']
+)
+
+question= "gimme the summary of this video"
+retrieved_docs= retriever.invoke(question)
+context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
+
+final_prompt = prompt.invoke({"context": context_text, "question": question})
+answer = model.invoke(final_prompt)
+print(answer.content)
+# # genearte the response
