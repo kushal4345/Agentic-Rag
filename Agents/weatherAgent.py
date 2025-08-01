0f1209c8f36bff261e688a7b60b744f7224dcd41
kushal4345 <jeeultimate4345@gmail.com>
2025-08-01 16:34:46 +0530

    langgraph start

--- Agents/weatherAgent.py
+++ Agents/weatherAgent.py
@@ -4,9 +4,19 @@ from langchain.agents import create_react_agent, AgentExecutor
 from langchain_community.tools import DuckDuckGoSearchRun
 from langchain import hub
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
+@tool
+def get_weather_data(city: str) -> str:
+  """
+  This function fetches the current weather data for a given city
+  """
+  url = f'https://api.weatherstack.com/current?access_key=62ba0b34f7b7aed696ed5f9492c5bf47&query={city}'
 
+  response = requests.get(url)
+
+  return response.json()
 # Setup tool and LLM
 search_tool = DuckDuckGoSearchRun()
 llm = AzureChatOpenAI(
@@ -22,19 +32,18 @@ prompt = hub.pull("hwchase17/react")
 # Create the agent
 agent = create_react_agent(
     llm=llm,
-    tools=[search_tool],
+    tools=[search_tool, get_weather_data],
     prompt=prompt
 )
 
 # Create AgentExecutor (FIXED: Removed `prompt`, added `tools`)
 agent_executor = AgentExecutor(
     agent=agent,
-    tools=[search_tool],
-    verbose=True
+    tools=[search_tool, get_weather_data],
+    verbose=True,
+handle_parsing_errors=True
 )
 
 # Invoke the agent
-response = agent_executor.invoke({"input": "gimme the three ways to go mumbai"})
-
+response = agent_executor.invoke({"input": "find the weather of datia and ts weather"})
 print(response['output'])
-
---
+++ Langgraph/coreConcept.py
@@ -0,0 +1,2 @@
+from langchain_community import hub
+from langchain_core.tools import tool
